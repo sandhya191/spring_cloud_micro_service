da17cf808707d1253f1cc92666aacfdb644a645a
Sandhya <sandhyaj224@gmail.com>
2018-03-08 23:13:37 -0500

    Refractor

--- src/main/java/com/examples/web/SpringCloudConfigServerApplication.java
+++ src/main/java/com/examples/web/SpringCloudConfigServerApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 
+ * @author Sandhya
+ * Spring Cloud Configuration Server Application of Project
+ *
+ */
+
 @EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
